19fb07cba574b878620bd164c8620cb6c99a7661
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-01 07:41:53 -0300

    creating a header, separating a “partial” for head and using include in the template

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'recipes/pages/home.html',
+                  context={'name': 'Rafael Amaral'},)
