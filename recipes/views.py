7b3b0b5438f0c93834d047e991ab0f829251b56a
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-08 08:54:58 -0300

    Showing recipe details page

--- recipes/views.py
+++ recipes/views.py
@@ -21,7 +21,10 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id, is_published=True,).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html',
-                  context={'recipe': make_recipe(),
+                  context={'recipe': recipe,
                            'is_detail_page': True,
                            })
