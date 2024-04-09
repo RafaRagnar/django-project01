bb351d8b93b322b5b5579664282c16b661933306
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-09 14:46:25 -0300

    Testing whether category and detail load the correct recipe

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse, resolve  # type: ignore
 from recipes import views
-from .test_recipe_base import RecipeTestBase, Recipe
+from .test_recipe_base import RecipeTestBase
 
 
 class RecipeViewsTest(RecipeTestBase):
@@ -8,27 +8,32 @@ class RecipeViewsTest(RecipeTestBase):
 
     def test_recipe_home_view_function_is_correct(self):
         ''' Test recipe home view function is correct'''
+
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
     def test_recipe_home_view_returns_status_code_200_ok(self):
         '''Test recipe home view returns status code 200 OK'''
+
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
 
     def test_recipe_home_view_loads_correct_template(self):
         '''Test recipe home view loads correct template '''
+
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
     def test_recipe_home_shows_no_recipes_found_if_no_recipes(self):
         '''Test recipe home shows no recipes found if no recipes'''
+
         response = self.client.get(reverse('recipes:home'))
         self.assertIn('<h1>No recipes found here 🥲</h1>',
                       response.content.decode('utf-8'))
 
     def test_recipe_home_template_loads_recipes(self):
         '''Test recipe home template loads recipes'''
+
         # Need a recipe for this test
         self.make_recipe()
 
@@ -42,6 +47,7 @@ class RecipeViewsTest(RecipeTestBase):
 
     def test_recipe_category_view_function_is_correct(self):
         ''' Test recipe category view function is correct'''
+
         view = resolve(reverse('recipes:category',
                        kwargs={'category_id': 1000}))
         self.assertIs(view.func, views.category)
@@ -49,18 +55,49 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_category_view_returns_404_if_no_recipes_found(self):
         """Test recipe category view returns status code 404 if no recipes
         found"""
+
         response = self.client.get(
             reverse('recipes:category', kwargs={'category_id': 1000}))
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_loads_recipes(self):
+        '''Test recipe category template loads recipes'''
+
+        needed_title = 'This is a category test'
+
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+
+        # Check if one recipe exists
+        self.assertIn(needed_title, content)
+
     def test_recipe_detail_view_function_is_correct(self):
         ''' Test recipe detail view function is correct'''
+
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
 
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         """Test recipe detail view returns status code 404 if no recipes
         found"""
+
         response = self.client.get(
             reverse('recipes:recipe', kwargs={'id': 1000}))
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_the_correct_recipes(self):
+        '''Test recipe detail template loads the correct recipes'''
+
+        needed_title = 'This is a detail page - It load one recipe'
+
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1}))
+        content = response.content.decode('utf-8')
+
+        # Check if one recipe exists
+        self.assertIn(needed_title, content)
