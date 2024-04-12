faae73e1193ed44633febabb744d5e1d651e8716
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-11 21:13:39 -0300

    Fixing slug field (SlugField) to unique (single value field)

--- recipes/models.py
+++ recipes/models.py
@@ -14,7 +14,7 @@ class Recipe(models.Model):
     """Class representing recipes"""
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
