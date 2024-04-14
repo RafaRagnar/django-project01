c159c9d096bdfb596ce291a2fb5edec83c373fa0
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-14 19:30:06 -0300

    Python-dotenv - environment variables (.env)

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,17 @@
+import os
 from django.shortcuts import render, get_list_or_404, get_object_or_404  # type: ignore
 from django.http.response import Http404    # type: ignore
 from django.db.models import Q  # type: ignore
-from django.core.paginator import Paginator  # type: ignore
 from recipes.models import Recipe
 from utils.pagination import make_pagination
+
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html',
                   context={'recipes': page_obj,
@@ -21,7 +23,7 @@ def category(request, category_id):
     recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id,
                               is_published=True).order_by('-id'))
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/category.html',
                   context={'recipes': page_obj,
@@ -55,7 +57,7 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
