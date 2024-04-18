cc4ab085e816fac4456df06e3a8887f10619b2e7
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-17 21:02:42 -0300

    Testing 'label' with unit testing

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -32,3 +32,16 @@ class AuthorRegisterFormUnitTest(TestCase):
         form = RegisterForm()
         current = form[field].field.help_text
         self.assertEqual(current, needed)
+
+    @parameterized.expand([
+        ('first_name', 'First name'),
+        ('last_name', 'Last name'),
+        ('username', 'Username'),
+        ('email', 'E-mail'),
+        ('password', 'Password'),
+        ('password2', 'Password2'),
+    ])
+    def test_fields_label(self, field, needed):
+        form = RegisterForm()
+        current = form[field].field.label
+        self.assertEqual(current, needed)
