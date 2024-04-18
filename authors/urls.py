c3d528b0c1e6654698f52254ab34484f82811b35
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-18 09:25:25 -0300

    Creating URLs, views and template for user login

--- authors/urls.py
+++ authors/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path    # type: ignore
 from . import views
 
 app_name = 'authors'
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
-    path('register/create/', views.register_create, name='create'),
+    path('register/create/', views.register_create, name='register_create'),
+    path('login/', views.login_create, name='login'),
+    path('login/create/', views.login_create, name='login_create'),
 ]
