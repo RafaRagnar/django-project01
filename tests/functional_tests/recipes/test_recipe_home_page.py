e29d2b08b0e64ad5bab21810fc970732ab23c9e2
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-19 17:32:16 -0300

    Using mixins to share methods between classes

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,8 +1,10 @@
 """Functional testing of the recipe home page"""
 from selenium.webdriver.common.by import By
+import pytest
 from tests.functional_tests.recipes.base import RecipeBaseFunctionalTest
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
 
     def test_recipe_home_page_without_recipes_not_found_message(self):
