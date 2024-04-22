f6a3191c9a209f0e5b9103932f8b1b2cfdc0121f
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-22 09:51:34 -0300

    Functional Testing for Accessibility Paging (ARIA)

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,13 +1,16 @@
 """Functional testing of the recipe home page"""
+from unittest.mock import patch
+
 import pytest
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from unittest.mock import patch
 from tests.functional_tests.recipes.base import RecipeBaseFunctionalTest
 
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    """Tests aimed at accessibility"""
     @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
@@ -40,4 +43,24 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         self.assertIn(
             title_needed,
             self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
+        )
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        self.make_recipe_in_batch()
+
+        # User opens the page
+        self.browser.get(self.live_server_url)
+
+        # The user sees that there is pagination and clicks on page 2
+        page2 = self.browser.find_element(
+            By.XPATH,
+            '//a[@aria-label="Go to page 2"]'
+        )
+        page2.click()
+
+        # The user clicks on page 2 and sees 2 more recipes
+        self.assertEqual(
+            len(self.browser.find_elements(By.CLASS_NAME, 'recipe')),
+            2
         )
