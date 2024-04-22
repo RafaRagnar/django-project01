1fc42df4decf1bc5cd13cff7f8dcc6154bf04c75
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-22 11:48:35 -0300

    Testing the login form

--- tests/functional_tests/authors/base.py
+++ tests/functional_tests/authors/base.py
@@ -1,5 +1,6 @@
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.by import By
 from utils.browser import make_chrome_browser
 
 
@@ -14,3 +15,9 @@ class AuthorsBaseTest(StaticLiveServerTestCase):
 
     def sleep(self, qtd=10):
         time.sleep(qtd)
+
+    def get_by_placeholder(self, web_element, placeholder):
+        return web_element.find_element(
+            By.XPATH,
+            f'//input[@placeholder="{placeholder}"]'
+        )
---
+++ tests/functional_tests/authors/test_authors_login.py
@@ -0,0 +1,35 @@
+import pytest
+
+from django.urls import reverse
+from django.contrib.auth.models import User
+from selenium.webdriver.common.by import By
+from .base import AuthorsBaseTest
+
+
+@pytest.mark.functional_test
+class AuthorsLoginTest(AuthorsBaseTest):
+    def test_user_valid_data_can_login_successfully(self):
+        string_password = 'pass'
+        user = User.objects.create_user(
+            username='my_user', password=string_password)
+
+        # User opens login page
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        # User sees login form
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        username_field = self.get_by_placeholder(form, 'Type your username')
+        password_field = self.get_by_placeholder(form, 'Type your password')
+
+        # The user enters their username and password
+        username_field.send_keys(user.username)
+        password_field.send_keys(string_password)
+
+        # The user submits the form
+        form.submit()
+
+        # The user sees the login success message and their name
+        self.assertIn(
+            f'Your are logged in with {user.username}.',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
