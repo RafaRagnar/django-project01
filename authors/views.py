8ee3dc9faeceb42cbf62de53d82abc5359773894
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-23 08:15:49 -0300

    Creating the recipe creation and editing form

--- authors/views.py
+++ authors/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from recipes.models import Recipe
+from authors.forms.registe_form import AuthorRecipeForm
+
 
 from .forms import LoginForm, RegisterForm
 
@@ -102,12 +104,19 @@ def dashboard_recipe_edit(request, id):
         is_published=False,
         author=request.user,
         pk=id,
-    )
+    ).first()
 
     if not recipe:
         raise Http404()
 
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        instance=recipe
+    )
+
     return render(
         request, 'authors/pages/dashboard_recipe.html',
-        context={}
+        context={
+            'form': form
+        }
     )
