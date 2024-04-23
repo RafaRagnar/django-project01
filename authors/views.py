1e25a9d3e8212526bfa0811935025c88fee3dd70
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-23 15:35:05 -0300

    Saving recipe edits in the database and request.FILES for cover

--- authors/views.py
+++ authors/views.py
@@ -111,8 +111,22 @@ def dashboard_recipe_edit(request, id):
 
     form = AuthorRecipeForm(
         data=request.POST or None,
+        files=request.FILES or None,
         instance=recipe
     )
+
+    if form.is_valid():
+        # Now the form is valid and I can try to save
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, 'Your recipe has been successfully saved!')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
 
     return render(
         request, 'authors/pages/dashboard_recipe.html',
