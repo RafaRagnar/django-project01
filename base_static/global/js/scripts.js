df849a6ce075567ef86808ae361b483db782e2b0
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-04-30 18:51:42 -0300

    Adding menu hide and show logic with JavaScript

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -1,17 +1,45 @@
-function my_scope() {
-    const forms = document.querySelectorAll('.form-delete');
+(() => {
+  const forms = document.querySelectorAll('.form-delete');
   
-    for (const form of forms) {
-      form.addEventListener('submit', function (e) {
-        e.preventDefault();
-  
-        const confirmed = confirm('Are you sure?');
-  
-        if (confirmed) {
-          form.submit();
-        }
-      });
-    }
+  for (const form of forms) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      const confirmed = confirm('Are you sure?');
+
+      if (confirmed) {
+        form.submit();
+      }
+    });
   }
-  
-  my_scope();
+})();    
+
+(() =>{
+  const buttonCloseMenu = document.querySelector('.button-close-menu');
+  const buttonShowMenu = document.querySelector('.button-show-menu');
+  const menuContainer = document.querySelector('.menu-container');
+
+  const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+  const menuHiddenClass = 'menu-hidden';
+
+  const closeMenu = () => {
+    buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+    menuContainer.classList.add(menuHiddenClass);
+  };
+
+  const showMenu = () => {
+    buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+    menuContainer.classList.remove(menuHiddenClass);
+  };
+
+  if (buttonCloseMenu) {
+    buttonCloseMenu.removeEventListener('click', closeMenu)
+    buttonCloseMenu.addEventListener('click', closeMenu);
+  }
+
+  if (buttonShowMenu) {
+    buttonShowMenu.removeEventListener('click', closeMenu);
+    buttonShowMenu.addEventListener('click', showMenu);
+  }
+
+})(); 
